62dce73ab3558a60bb0966dc62ed3a386836f231
owen chan <cycowen@163.com>
2014-08-25 16:53:27 +0800

    demo1.udpchat
    有bug

---
+++ Demo1.udpchat/Demo1.udpchat/mainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Demo1.udpchat
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(273, 340);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(26, 21);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(322, 21);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "255.255.255.255";
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(26, 79);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistory.Size = new System.Drawing.Size(322, 188);
+            this.txtHistory.TabIndex = 1;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(26, 291);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(322, 21);
+            this.txtChat.TabIndex = 1;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 389);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnSend);
+            this.Name = "mainForm";
+            this.Text = "聊天中...";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.TextBox txtChat;
+    }
+}
+
---
+++ Demo1.udpchat/Demo1.udpchat/mainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Demo1.udpchat
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            UdpClient uc = new UdpClient();
+            string txtIP = this.txtIP.Text;
+            string chat = "PUBLIC|" + this.txtChat.Text + "|陈宇川";
+            byte[] bchat = Encoding.Default.GetBytes(chat);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(txtIP), 9527);
+            uc.Send(bchat,bchat.Length,ipep);
+            this.txtChat.Text = "";
+
+        }
+        private void listen()
+        {
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any,9527);
+                byte[] bchat = uc.Receive(ref ipep);
+                string chat = Encoding.Default.GetString(bchat);
+                string[] s = chat.Split('|');
+                if (s.Length == 3 && s[0] =="PUBLIC")
+                {
+                    string schat = s[1];
+                    string sname = s[2];
+                    this.txtHistory.Text += sname + ":" + "\r\n";
+                    this.txtHistory.Text += schat + "\r\n";
+                }
+                else if (s[0] == "INROOM" && s.Length == 2)
+                {
+                    string sname = s[1];
+                    this.txtHistory.Text += sname + "上线了" + "\r\n";
+                }
+                else
+                {
+                    return;
+                }
+                
+            }
+        }
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            UdpClient uc = new UdpClient();
+            string txtIP = this.txtIP.Text;
+            string chat = "INROOM|陈宇川";
+            byte[] bchat = Encoding.Default.GetBytes(chat);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(txtIP), 9527);
+            uc.Send(bchat, bchat.Length, ipep);
+            mainForm.CheckForIllegalCrossThreadCalls = false;
+            Thread th = new Thread(new ThreadStart(listen));
+            th.Start();
+
+        }
+    }
+}
